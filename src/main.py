ee5ed772aec482fffc9badb58525fbd7a7deff1b
a.dolgov <dolgov_am@mail.ru>
2025-02-12 10:20:45 +0300

    add rout for search

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
+templates = Jinja2Templates(directory='templates')
 
 main_app = FastAPI()
 
+
 @main_app.get('/')
-def hello_world():
-    return {'success': "true"}
+def start(request: Request):
+    return templates.TemplateResponse('start.html', {'request': request})
+
+
+@main_app.get('/ai_search')
+def search_ai(request: Request):
+    return templates.TemplateResponse('search.html', {'request': request})
 
 
 if __name__ == "__main__":
